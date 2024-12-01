545d850258a914562dadf14bef9a27c146e9bb38
Jared M Ashcraft <jaredmashcraft@gmail.com>
2024-12-01 17:47:55 -0500

    :bricks: Set up modules

---
+++ src/day1/mod.rs
@@ -0,0 +1,7 @@
+pub fn day1_1() {
+    println!("day1_1");
+}
+
+pub fn day1_2() {
+    println!("day1_2");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod day1;
+
 fn main() {
     println!("Hello, world!");
+    day1::day1_1();
+    day1::day1_2();
 }
